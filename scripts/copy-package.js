4d301c50711fff857187d0e80b58b9dab73e550a
ramonornela <ramon.ornela@gmail.com>
2016-10-26 14:44:08 -0200

    chore(): ajuste para publicacao do modulo

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
 var process = require('process');
 var path = require('path');
+var process = require('process');
 
-var packageJson = require(path.join('..', 'package.json'));
+var originalPath = path.join(process.cwd(), 'package.json');
+var tempPath = path.join(process.cwd(), 'package_tmp.json');
+var packageJson = require(originalPath);
+var readme = fs.readFileSync(path.join(process.cwd(), 'README.md'));
 
 if (! packageJson) {
   throw new Error('Can\'t find package json');
@@ -12,4 +16,6 @@ delete packageJson.devDependencies;
 delete packageJson.dependencies;
 delete packageJson.scripts;
 
-fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2));
+fs.renameSync(originalPath, tempPath);
+fs.writeFileSync(originalPath, JSON.stringify(packageJson, null, 2));
+fs.renameSync(tempPath, originalPath);
