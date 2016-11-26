88f42baa3e7425c3f2df2f0b04ab60293b508557
ramonornela <ramon.ornela@gmail.com>
2016-11-26 18:10:47 -0200

    test(config): improvements tests config and env

--- test/config-spec.ts
+++ test/config-spec.ts
@@ -1,24 +1,39 @@
 import 'zone.js';
+import 'reflect-metadata';
 import 'zone.js/dist/proxy';
 import 'zone.js/dist//sync-test';
 import 'zone.js/dist/jasmine-patch';
-import { TestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import { ConfigModule } from '../src';
+import { TestBed, inject } from '@angular/core/testing';
+import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { ConfigModule, Config, ConfigToken, EnvToken } from '../src';
 
-describe('factories json', () => { 
+TestBed.initTestEnvironment(
+  ConfigModule,
+  platformBrowserDynamicTesting()
+);
+
+describe('Module Config', () => {
   beforeEach(() => {
-    TestBed.initTestEnvironment(
-      BrowserDynamicTestingModule,
-      platformBrowserDynamicTesting()
-    );
+    let data = {
+      'my-config': 'value'
+    };
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: ConfigToken, useValue: data },
+        { provide: EnvToken, useValue: 'dev' }
+      ]
+    });
   });
 
-  afterEach(() => {
-    TestBed.resetTestEnvironment();
-  });
+  it('Env', inject([Config], (config: Config) => {
+    expect(config.getEnv()).toEqual('dev');
+    config.setEnv('prod');
+    expect(config.getEnv()).toEqual('prod');
+  }));
 
-  it('foo', () => {
-
-  });
+  it('Get config', inject([Config], (config: Config) => {
+    expect(config.get('my-config')).toEqual('value');
+    config.set('my-other-config', 'value-config');
+    expect(config.get('my-other-config')).toEqual('value-config');
+  }));
 });
