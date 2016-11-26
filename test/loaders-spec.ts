5336b83493e4d1751980db7446cd5bc1c5e57575
ramonornela <ramon.ornela@gmail.com>
2016-11-26 09:27:47 -0200

    test(loader): added test object

--- test/loaders-spec.ts
+++ test/loaders-spec.ts
@@ -1,5 +1,5 @@
 import { MockBrowserXhr } from './mocks';
-import { loaderFactory, Json } from '../src';
+import { loaderFactory, Json, ConfigBase } from '../src';
 
 describe('loaders', () => {
   describe('factories type invalids', () => {
@@ -10,6 +10,11 @@ describe('loaders', () => {
 
     it('invalid type null', () => {
       expect(() => loaderFactory(null))
+        .toThrow(new Error('Invalid configuration'));
+    });
+
+    it('invalid type number', () => {
+      expect(() => loaderFactory(1))
         .toThrow(new Error('Invalid configuration'));
     });
   });
@@ -23,6 +28,13 @@ describe('loaders', () => {
     it('test file local', () => {
       var jsonLoader = loaderFactory('assets/data.json', new MockBrowserXhr());
       expect(jsonLoader instanceof Json).toBeTruthy();
+    });
+  });
+
+  describe('factory object', () => {
+    it('test object', () => {
+      var jsonLoader = loaderFactory({});
+      expect(jsonLoader instanceof ConfigBase).toBeTruthy();
     });
   });
 });
