b6f85843547e2d2e6b759f4b3c405d1852912351
ramonornela <ramon.ornela@gmail.com>
2016-11-26 09:04:18 -0200

    test(loaders): improvements mock and added test loader json

--- test/loaders-spec.ts
+++ test/loaders-spec.ts
@@ -1,8 +1,8 @@
 import { MockBrowserXhr } from './mocks';
-import { loaderFactory } from '../src';
+import { loaderFactory, Json } from '../src';
 
 describe('loaders', () => {
-  describe('factories tests', () => {
+  describe('factories type invalids', () => {
     it('invalid type string', () => {
       expect(() => loaderFactory('{}', new MockBrowserXhr()))
         .toThrow(new Error('Invalid configuration'));
@@ -11,6 +11,18 @@ describe('loaders', () => {
     it('invalid type null', () => {
       expect(() => loaderFactory(null))
         .toThrow(new Error('Invalid configuration'));
+    });
+  });
+
+  describe('factories json', () => {
+    it('test file external http', () => {
+      var jsonLoader = loaderFactory('http://example.com/data', new MockBrowserXhr());
+      expect(jsonLoader instanceof Json).toBeTruthy();
+    });
+
+    it('test file local', () => {
+      var jsonLoader = loaderFactory('assets/data.json', new MockBrowserXhr());
+      expect(jsonLoader instanceof Json).toBeTruthy();
     });
   });
 });
--- test/mocks.ts
+++ test/mocks.ts
@@ -4,9 +4,29 @@ import { BrowserXhr } from '@angular/http';
 
 @Injectable()
 export class MockBrowserXhr extends BrowserXhr {
+
+  xhr: any;
+
   constructor() {
   	super();
   }
 
-  build(): any {}
+  build(): any {
+    this.xhr = new XHRProxy();
+    return this.xhr;
+  }
 }
+
+export var XHRProxy = function() {
+  this.uri = null;
+}
+
+XHRProxy.prototype.responseType = null;
+
+XHRProxy.prototype.open = function(method, uri, sync) {
+  this.file = uri;
+}
+
+XHRProxy.prototype.addEventListener = function(event, callback) {}
+
+XHRProxy.prototype.send = function(data) {}
